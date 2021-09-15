0bc13614965bcb1da5c58acaa2141b238cc5e65b
Estevan Sinarta <estevan_sinarta@yahoo.ca>
2021-09-15 12:16:59 -0700

    Displayed date, copyright, explanation for photos

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const NASA_API_KEY = process.env.REACT_APP_NASA_API_KEY;
 const NASA_APOD_URL = 'https://api.nasa.gov/planetary/apod';
 
 function App() {
-  const [photos, setPhotos] = useState({});
+  const [photos, setPhotos] = useState([]);
 
   useEffect(() => {
     const getPhotos = async () => {
@@ -16,7 +16,6 @@ function App() {
         });
       return res;
     };
-
     getPhotos();
   },[]);
 
@@ -30,6 +29,13 @@ function App() {
           {photos.map((photo, index) => (
             <div key={index}>
               <img src={photo.hdurl} alt={photo.title} />
+              {photo.copyright ? 
+                <p>Copyright: {photo.copyright}</p>
+                :
+                <p>Copyright: None</p>
+              }
+              <p>Date: {photo.date}</p>
+              <p>{photo.explanation}</p>
             </div>
           ))}
         </div>
